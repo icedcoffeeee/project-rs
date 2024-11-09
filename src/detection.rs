75caedb738acd82571a6df92be8834beeea0e1ab
icedcoffeeee <hazim.saharuddin@gmail.com>
2024-11-10 00:58:41 +0800

    enable flags

--- src/detection.rs
+++ src/detection.rs
@@ -4,7 +4,7 @@ use mpsc::{Receiver, Sender};
 type Detections = (Vector<i32>, Vector<f32>, Vector<Rect_<i32>>);
 
 pub fn initialize_thread(r_feed: Receiver<Mat>, t_detections: Sender<Detections>) {
-    let mut model = dnn::DetectionModel::new("yolo/yolov3.weights", "yolo/yolov3.cfg").unwrap();
+    let mut model = dnn::DetectionModel::new("data/yolov3.weights", "data/yolov3.cfg").unwrap();
     let mut init = false;
     loop {
         let feed: Mat = match r_feed.try_recv() {
@@ -25,14 +25,29 @@ pub fn initialize_thread(r_feed: Receiver<Mat>, t_detections: Sender<Detections>
     }
 }
 
-pub fn draw_bounding_boxes(mat: &mut Mat, detections: &Detections, classes: &Vec<&str>) {
+pub fn draw_bounding_boxes(
+    mat: &mut Mat,
+    detections: &Detections,
+    classes: &mut Option<Vec<String>>,
+) {
+    if classes.is_none() {
+        let raw = fs::read("data/yolov3.txt").unwrap();
+        let string: Vec<String> = String::from_utf8(raw)
+            .unwrap()
+            .split("\n")
+            .map(|x| x.to_string())
+            .collect();
+        *classes = Some(string);
+    }
+    let classes = classes.as_ref().unwrap();
+
     let (class_ids, scores, rects) = detections;
     let mut indices = Vector::<i32>::new();
     dnn::nms_boxes_def(&rects, &scores, 0.5, 0.1, &mut indices).unwrap();
 
     for i in indices {
         let rect: Rect = rects.to_vec()[i as usize];
-        let label = classes[class_ids.to_vec()[i as usize] as usize];
+        let label = classes[class_ids.to_vec()[i as usize] as usize].as_str();
         let color: VecN<f64, 4> = [0., 0., 255., 255.].into();
         imgproc::rectangle_def(mat, rect, color).unwrap();
 
